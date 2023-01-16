af520756f5bc8da3bc7424d2180eec5503051140
Adish Jain <adish@Adishs-MBP-2.lan>
2023-01-16 14:01:14 -0800

    mobile UX WIP

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
-import "../App.css"
+import NavbarDropdown from "./NavbarDropdown";
+import "../App.css";
 
 const Layout = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 700);
+  window.addEventListener(
+    "resize",
+    () => {
+      setIsMobile(window.innerWidth < 700);
+    },
+    []
+  );
   return (
     <div class="page-container">
-      <Navbar />
+      {isMobile ? <NavbarDropdown /> : <Navbar />}
       <Outlet />
     </div>
   );
---
+++ src/pages/NavbarDropdown.js
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import "../NavbarDropdown.css";
+import logo from "../logo.svg";
+import { useLocation } from "react-router-dom";
+
+function NavbarDropdown() {
+  const [opened, toggleOpen] = useState(false);
+  const location = useLocation();
+  return (
+    <div className={"NavbarDropdown"}>
+      {/* <Logo goToIndex={props.goToIndex} /> */}
+      <div className={"dropdown"}>
+        <button onClick={(e) => toggleOpen(!opened)} className={"dropbtn"}>
+          ☰
+        </button>
+        <div className={"dropdownContent"}>
+          {opened ? (
+              <ul>
+                <li>
+                  <Link
+                    to="/story"
+                    class={location.pathname === "/story" ? "active" : ""}
+                  >
+                    Our Story
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/events"
+                    class={location.pathname === "/events" ? "active" : ""}
+                  >
+                    Events
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/around"
+                    class={location.pathname === "/around" ? "active" : ""}
+                  >
+                    Getting around
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/stay"
+                    class={location.pathname === "/stay" ? "active" : ""}
+                  >
+                    Where to Stay
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/sightseeing"
+                    class={location.pathname === "/sightseeing" ? "active" : ""}
+                  >
+                    Sightseeing
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/rsvp"
+                    class={location.pathname === "/rsvp" ? "active" : ""}
+                  >
+                    RSVP
+                  </Link>
+                </li>
+              </ul>
+          ) : (
+            <div></div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default NavbarDropdown;
